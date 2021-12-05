a0563d67219baf74b6ea0b20886089aea7bfffb4
Robert-byte-ai <saakyan-robert@rambler.ru>
2021-12-05 13:07:21 +0300

    Models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,132 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+from .utils import random_string_generator
+
+User = get_user_model()
+
+
+class Seller(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    @property
+    def count_ads(self):
+        count = Ad.objects.filter(seller=self.user).count()
+        if count:
+            return count
+        return 0
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Продавец'
+        verbose_name_plural = 'Продавцы'
+
+    def __str__(self):
+        return self.user
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Имя категории'
+    )
+
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        null=True,
+        verbose_name='Slug категории',
+        db_index=True
+    )
+
+    def unique_slug_generator(self, instance, new_slug=None):
+        if new_slug is not None:
+            slug = new_slug
+        else:
+            slug = slugify(instance.title)
+
+        Category = instance.__class__
+        qs_exists = Category.objects.filter(slug=slug).exists()
+        if qs_exists:
+            new_slug = "{slug}-{randstr}".format(
+                slug=slug,
+                randstr=random_string_generator(length=4)
+            )
+            return self.unique_slug_generator(instance, new_slug=new_slug)
+        return slug
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        db_index=True,
+    )
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200,
+        db_index=True,
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='ads',
+        verbose_name='Категория'
+    )
+
+    seller = models.ForeignKey(
+        Seller,
+        on_delete=models.CASCADE,
+        related_name='ads',
+        verbose_name='Продавец'
+    )
+
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации'
+    )
+
+    edited_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата редактирования'
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='ads',
+        verbose_name='Теги',
+    )
+
+    class Meta:
+        ordering = ('-pk',)
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+    def __str__(self):
+        return self.name
--- main/utils.py
+++ main/utils.py
@@ -1,2 +1,11 @@
+import random
+import string
+
+
 def get_filename(filename, request):
     return filename.upper()
+
+
+def random_string_generator(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
