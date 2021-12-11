9aeef14c04109f2e6c40a39ade7ed565c5a6420c
Robert-byte-ai <saakyan-robert@rambler.ru>
2021-12-11 12:26:02 +0300

    custom tags and filters

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from constance import config
+import random
 
 
 def index(request):
     context = {
         "turn_on_block": config.MAINTENANCE_MODE,
-        "username": request.user.username
+        "username": request.user.username,
+        'notifications': random.randint(0, 100),
+        'hello': 'Привет, мир!'
     }
     return render(request, 'index.html', context)
