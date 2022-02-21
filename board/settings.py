56d124d28f37b65b7d354dc2bc3a0fcea7de8a99
Robert-byte-ai <saakyan-robert@rambler.ru>
2022-02-21 18:35:17 +0300

    docker

--- board/settings.py
+++ board/settings.py
@@ -166,7 +166,7 @@ CELERY_TIMEZONE = 'Europe/Moscow'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/',
+        'LOCATION': 'redis://redis:6379/',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
