a2c7eea99b39b9e88746037c8bc72e8b45aa45ac
Robert-byte-ai <saakyan-robert@rambler.ru>
2022-03-14 12:57:06 +0300

    id

--- api/serializers.py
+++ api/serializers.py
@@ -1,22 +1,10 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
 
 from main.models import Ad, Category
 
 
 class AdSerializer(serializers.ModelSerializer):
-    category = SlugRelatedField(
-        slug_field='name',
-        queryset=Category.objects.all()
-    )
 
     class Meta:
-        fields = (
-            'seller',
-            'pub_date',
-            'category',
-            'tags',
-            'price',
-            'text'
-        )
+        fields = '__all__'
         model = Ad
