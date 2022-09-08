03bf7d893cf7d48a234efa2d75d4b762a06a2966
robert <saakyan-robert@rambler.ru>
2022-09-08 17:55:47 +0300

    delete task

--- main/tasks.py
+++ main/tasks.py
@@ -4,10 +4,8 @@ import random
 from dotenv import load_dotenv
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
-from datetime import timedelta
 
-from main.models import Ad, Subscription, User, Seller, SMSLog
+from main.models import Seller, SMSLog
 from board.celery import app
 
 load_dotenv()
@@ -21,24 +19,6 @@ def supper_sum(x: int, y: int) -> int:
 @app.task
 def hello() -> str:
     return 'Hello, world'
-
-
-@app.task
-def ads_message() -> None:
-    ads_for_week = Ad.objects.filter(
-        pub_date__range=(
-            (timezone.now() - timedelta(days=7)),
-            timezone.now()
-        )
-    )
-    emails = [
-        user.email for user in User.objects.all()
-        if Subscription.objects.filter(user=user).exists()
-    ]
-    ads = [ad.name for ad in ads_for_week]
-    if len(ads_for_week) > 0:
-        return send_message(f'New ads: {" ".join(ads)}', emails)
-    return send_message('There are no ads', emails)
 
 
 def send_message(message: str, emails: list) -> None:
